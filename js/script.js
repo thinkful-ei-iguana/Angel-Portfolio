96b9129aee8dc9d62e8ebdbf99e23d7aeef8d9e5
Angel Alicea <ala31996@gmail.com>
2021-01-09 13:19:04 -0500

    changed background

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 const selectElement = (element) => document.querySelector(element);
 
 //Open Nav Menu On Click
-selectElement('.open').addEventListener('click', () => {
-  selectElement( '.nav-list').classList.add('active');
+selectElement(".open").addEventListener("click", () => {
+  selectElement(".nav-list").classList.add("active");
 });
 
 //Close Nav Menu On Click
-selectElement( '.close').addEventListener('click', () => {
-  selectElement('.nav-list').classList.remove( 'active');
+selectElement(".close").addEventListener("click", () => {
+  selectElement(".nav-list").classList.remove("active");
 });
+
+VANTA.NET("#hero");

e7612f27c43354319b7594044fe11826cb234b00
aliceaa396 <ala31996@gmail.com>
2021-03-05 12:08:43 -0500

    changed backgrounds v.1.5

--- js/script.js
+++ js/script.js
@@ -10,3 +10,52 @@ selectElement('.open').addEventListener('click', () => {
 selectElement( '.close').addEventListener('click', () => {
   selectElement('.nav-list').classList.remove( 'active');
 });
+
+//Dynamic Background Info
+VANTA.TOPOLOGY({
+  el: "#vantajs",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x910da2,
+  backgroundColor: 0x51414
+});
+
+
+VANTA.NET({
+  el: "#exp",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  points: 8.00,
+  maxDistance: 30.00,
+  spacing: 16.00,
+  color: 0x910da2,
+  backgroundColor: 0x51414
+})
+
+VANTA.WAVES({
+  el: "#footer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x250e2d
+})
+
+
+/* code highlight */
+$(document).ready(function(){
+  hljs.initHighlightingOnLoad();
+});
